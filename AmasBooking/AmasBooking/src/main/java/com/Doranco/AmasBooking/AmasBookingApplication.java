542255576747bc30f455e055e2d25f7aca29c79a
mariama2244 <119751878+mariama2244@users.noreply.github.com>
2024-05-28 08:01:04 +0200

    delete file cas

---
+++ AmasBooking/AmasBooking/src/main/java/com/Doranco/AmasBooking/AmasBookingApplication.java
@@ -0,0 +1,13 @@
+package com.Doranco.AmasBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmasBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmasBookingApplication.class, args);
+	}
+
+}
---
+++ AmasBooking/AmasBooking/src/test/java/com/Doranco/AmasBooking/AmasBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Doranco.AmasBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmasBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
